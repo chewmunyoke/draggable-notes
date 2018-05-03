b32b7b02cbd8c8b81645f8dfb903e6e8862d178a
Angeline Chew <chewmunyoke@users.noreply.github.com>
2018-05-04 02:39:19 +0800

    Dev commit

---
+++ dev/misc/header.vue
@@ -0,0 +1,41 @@
+<template id="header-component">
+	<header id="header" class="header" :class="headerClass">
+		<h1>{{ appTitle }}</h1>
+		<span class="message">{{ appMessage }}</span>
+		<button class="dragger-switch"
+			:class="draggerButtonClass"
+			v-on:click="draggerClickHandler">
+			Switch view
+		</button>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: 'header-component',
+		props: ['state'],
+		data() {
+			return {
+				appTitle: 'Draggable Notes',
+				appMessage: 'This mobile version does not have the slideshow switch'
+			}
+		},
+		computed: {
+			headerClass: function() {
+				return {
+					'hide': this.state.appIsShowContent
+				};
+			},
+			draggerButtonClass: function() {
+				return {
+					'view-max': this.state.draggerButtonIsToggled
+				}
+			}
+		},
+		methods: {
+			draggerClickHandler: function(event) {
+				app.toggle();
+			}
+		}
+	}
+</script>
---
+++ dev/misc/slideshow.vue
@@ -0,0 +1,31 @@
+<template id="slideshow-component">
+	<div class="slideshow">
+		<div class="dragdealer dragger">
+			<div class="handle">
+				<div class="slide">
+					<div class="note">
+						<div class="title-container">
+							<span class="title"></span>
+							<span class="subtitle"></span>
+							<span class="timestamp"></span>
+						</div>
+						<div class="content">
+						</div>
+					</div>
+					<div class="content-switch-container">
+						<div class="content-switch-wrapper">
+							<button class="content-switch"></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['state', 'options', 'note'],
+		name: 'slideshow-component'
+	}
+</script>
